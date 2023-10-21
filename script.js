28734073a0913bd0305f1c52d46a7a8f2f1f9c2a
AminBenAttia <aminbenattia47@gmail.com>
2023-10-21 11:22:08 +0100

    sauvegarde_des_preferance

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function convertTemperature() {
   var celsius = parseFloat(document.getElementById("celsius").value);
-  var fahrenheit = (celsius * 9/5) + 32;
+  var fahrenheit = (celsius * 9) / 5 + 32;
   document.getElementById("fahrenheit").value = fahrenheit;
+}
+function getDefaultUnit() {
+  let unit = localStorage.getItem("unit");
+  if (!unit) {
+    localStorage.setItem("unit", "Celsius");
+    unit = "Celsius";
+  }
+  document.getElementById("unitSelect").childNodes.forEach((option) => {
+    if (option.value === unit) {
+      option.setAttribute("selected", true);
+    }
+  });
+}
+function setDefaultUnit() {
+  localStorage.setItem("unit", document.getElementById("unitSelect").value);
 }
