9c36826028cd49a4d07db83abd2495babe7fda96
BOUHMIDDD <ahmed.bou7.kammoun@gmail.com>
2023-10-21 11:36:24 +0100

    AhKa addition image de conversion

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function convertTemperature() {
   var celsius = parseFloat(document.getElementById("celsius").value);
   var fahrenheit = (celsius * 9/5) + 32;
-  document.getElementById("fahrenheit").value = fahrenheit;
+  document.getElementById("fahrenheit").value = fahrenheit;  
 }
+
+
