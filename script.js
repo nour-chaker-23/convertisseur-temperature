8536f5ba710f7b0c6f04a4c1826b9f696fdf3608
Alassan00 <elhassenba00@gmail.com>
2023-10-21 11:29:29 +0100

    l'ajout d'Historique des conversion

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+let histoCoversion = [];
+
 function convertTemperature() {
   var celsius = parseFloat(document.getElementById("celsius").value);
   var fahrenheit = (celsius * 9/5) + 32;
   document.getElementById("fahrenheit").value = fahrenheit;
+
+  const recordCoversion = `${celsius} °C = ${fahrenheit} °F`;
+  histoCoversion.push(recordCoversion);
+
+  updateHistoCoversion();
+}
+
+
+function updateHistoCoversion() {
+  const historyElement = document.getElementById("histoCoversion");
+
+  historyElement.innerHTML = "";
+
+  for (const record of histoCoversion) {
+    const p = document.createElement("p");
+    p.textContent = record;
+    historyElement.appendChild(p);
+  }
 }
