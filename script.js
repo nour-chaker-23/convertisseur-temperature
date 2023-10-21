a6d3ef274f1370cce858539acb991eb01aaadfb6
mohamed drira <drira2003@gmail.com>
2023-10-21 11:22:21 +0100

    convert unit bidirection

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+var unit="fahrenheit";
 function convertTemperature() {
-  var celsius = parseFloat(document.getElementById("celsius").value);
-  var fahrenheit = (celsius * 9/5) + 32;
-  document.getElementById("fahrenheit").value = fahrenheit;
+  var first = parseFloat(document.getElementById("first field").value);
+  if (unit=="fahrenheit"){
+    var second = (first * 9/5) + 32;
+    document.getElementById("second field").value = second;
+  }else{
+    var second = (first -32)*(5/9)
+    document.getElementById("second field").value = second;
+  }
+  
+}
+function change(){
+  if (unit=="fahrenheit"){
+    unit="celsius"
+    document.getElementById("second label").innerHTML="Celsius:";
+    document.getElementById("first label").innerHTML="Fahrenheit:"
+  }else{
+    unit="fahrenheit"
+    document.getElementById("first label").innerHTML="Celsius:";
+    document.getElementById("second label").innerHTML="Fahrenheit:"
+  }
 }
