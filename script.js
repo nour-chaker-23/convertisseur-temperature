ba5cec2f3ff6345152b9175486c1efc9e2d348a4
AlaaBenAmeur <alaa.benameur@gmail.com>
2023-10-21 11:22:26 +0100

    Copy to Clipboard Button added

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function convertTemperature() {
   var fahrenheit = (celsius * 9/5) + 32;
   document.getElementById("fahrenheit").value = fahrenheit;
 }
+
+function copyText(){
+  var Text = document.getElementById("fahrenheit"); 
+  Text.select(); 
+  navigator.clipboard.writeText(Text.value); 
+}
