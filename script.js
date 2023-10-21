6c7ba0bb5f923dde3e6089f54e0ea5b66f1dcb5b
Shaima Hamila <hamilachaima18@gmail.com>
2023-10-21 11:27:34 +0100

    addMultipleUnits

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function convertTemperature() {
   var celsius = parseFloat(document.getElementById("celsius").value);
-  var fahrenheit = (celsius * 9/5) + 32;
-  document.getElementById("fahrenheit").value = fahrenheit;
+  var selectedUnit = document.getElementById("unitChosen").value;
+  switch (selectedUnit) {
+    case "fahrenheit":
+      var unitConverted = (celsius * 9) / 5 + 32;
+      break;
+    case "kelvin":
+      var unitConverted = celsius + 273.15;
+  }
+
+  document.getElementById("unitValue").value = unitConverted;
 }
