073afd8d77812ad212063e5a3bfaa8f260226008
Nouha <nouhahajlaoui11@gmail.com>
2023-10-24 20:42:10 +0100

    notification d'erreur

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function convertTemperature() {
   var celsius = parseFloat(document.getElementById("celsius").value);
+
+  if (isNaN(celsius) || celsius > 5) {
+    alert("Erreur: Veuillez entrer une valeur valide et inferieure ou egale a 5.");
+    return;
+  }
   var fahrenheit = (celsius * 9/5) + 32;
   document.getElementById("fahrenheit").value = fahrenheit;
 }
